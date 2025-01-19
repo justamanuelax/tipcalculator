c40292832d7027a1f383981ae479a18dbbc65e16
Open_Source_Lover <justamanuelax@gmail.com>
2025-01-19 19:25:05 +0200

    The code has been corrected (Tip Calculator)

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,14 @@ function TipCalculator(){
     setPercentage2(0);
   }
 
-  const tip = (bill * ((percentage1 + percentage2)/2/100));
+  const tip = Math.round((bill * ((percentage1 + percentage2) / 2 / 100)) * 100) / 100;
 
   return(
     <div>
-      <BillInput bill={bill} setBill={setBill} />
-      <SelectPercentages percentage1={percentage1} setPercentage1={setPercentage1}>How did you like the services?</SelectPercentages>
-      <SelectPercentages percentage2={percentage2} setPercentage2={setPercentage2}>How did your friend like the service?</SelectPercentages>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentages percentage={percentage1} setPercentage={setPercentage1}>How did you like the services?</SelectPercentages>
+      <br />
+      <SelectPercentages percentage={percentage2} setPercentage={setPercentage2}>How did your friend like the service?</SelectPercentages>
       {bill > 0 && (
         <>
         {" "}
@@ -40,36 +41,46 @@ function TipCalculator(){
 
 }
 
-function BillInput(bill, setBill){
+function BillInput({bill, onSetBill}){
+  BillInput.propTypes ={
+    bill: PropTypes.string,
+    onSetBill: PropTypes.string
+  }
   return(
     <div>
       <label>
         How much was the bill?
       </label>
-      <input type="text" placeholder="Bill value" value={bill} onChange={(e) => setBill(+e.target.value)}/>
+      <input type="text" placeholder="Bill value" value={bill} onChange={e => onSetBill(Number(e.target.value))}/>
     </div>
   )
 }
 
-function SelectPercentages({children}){
+function SelectPercentages({children, percentage, setPercentage}){
   SelectPercentages.propTypes = {
-    children: PropTypes.array
+    children: PropTypes.string,
+    percentage: PropTypes.number,
+    setPercentage: PropTypes.number
   }
   return(
   <>
   <label>{children}</label>
-  <select name="" id="">
-  <option value="0"> </option>
-  <option value="5"> </option>
-  <option value="10"> </option>
-  <option value="20"> </option>
+  <select value={percentage} onChange={(e) => setPercentage(+(e.target.value))}>
+  <option value="0">Not Good (0%) </option>
+  <option value="5">Okay (5%) </option>
+  <option value="10">Really Good (10%) </option>
+  <option value="20">Absolutely Amazing (20%)</option>
   </select>
 
   </>
 )
 }
 
-function Output(bill, tip){
+function Output({bill, tip}){
+  Output.propTypes = {
+    bill: PropTypes.string, 
+    tip: PropTypes.number
+  }
   return(
     <>
     <h3>You pay ${bill + tip} (${bill} + ${tip} tip)</h3>
